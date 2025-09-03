7c57001a1b8d43b817216e5c3d0afa158cd66544
Babyjoe-27 <josephpefoho@gmail.com>
2025-09-03 16:29:07 +0100

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+// function myFun() {
+//             document.getElementById("demo")
+//             .innerHTML= "my meal"; 
+//             document.getElementById("demo").style.color ="red";
+//         }
+
+//         //basic concepts of Javascript
+//         let num1 = prompt("Enter a first number");
+//         let num2 = prompt("Enter a second number");
+
+//         let sum = Number(num1) + Number(num2);
+//         alert(sum)
+
+//compering 3 numbers
+         let a = 10;
+         let b = 110;
+         let c = 5;
+    if (a>b & a>c){
+        console.log("A is the greatest")
+    }else if(b>c){
+        console.log("B is the greatest")
+    } else {
+        console.log("C is the greatest")
+    }
